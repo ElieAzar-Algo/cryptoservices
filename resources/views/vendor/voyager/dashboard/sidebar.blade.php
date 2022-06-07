de7396a8eb9d4f6b4504c9292f3266572f1b1b5d
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2022-06-07 21:17:18 +0300

    page:media, partials,roles, admin-settings, posts, tools, users, index-master-profile-login-dimmers-alerts

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -19,6 +19,7 @@
                  style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;">
                 <div class="dimmer"></div>
                 <div class="panel-content">
+                    
                     <img src="{{ $user_avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
                     <h4>{{ ucwords(Auth::user()->name) }}</h4>
                     <p>{{ Auth::user()->email }}</p>
@@ -30,6 +31,7 @@
 
         </div>
         <div id="adminmenu">
+        {{-- this is the menu --}}
             <admin-menu :items="{{ menu('admin', '_json') }}"></admin-menu>
         </div>
     </nav>
